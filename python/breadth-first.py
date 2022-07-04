4102b7b403524862ef6af86e9316e397d985c530
Tom Estabrook <thomas.r.estabrook@gmail.com>
2022-07-04 13:30:12 -0400

    Add: iterative breadth first algorithm

--- python/breadth-first.py
+++ python/breadth-first.py
@@ -1,12 +1,9 @@
 def breadthFirst(graph: dict(), node: str) -> None:
-    # Keep track of nodes to traverse in a first in / first out fashion
     queue = [ node ]
-    # Keep visting nodes until there are not more in the graph
     while len(queue) > 0:
         current = queue.pop(0)
         print(current)
 
-        #For every neighbor of this node add it to a list to check
         for neighbor in graph[current]:
             queue.append(neighbor)
 
