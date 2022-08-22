94ef47dc47fe1016de168b07b94fccdd90f0e926
Tom Estabrook <thomas.r.estabrook@gmail.com>
2022-08-22 12:55:46 -0400

    refactor: use getter and setter methods

--- python/data-structures/node.py
+++ python/data-structures/node.py
@@ -6,24 +6,33 @@ class Node:
     link_node: A reference to a Node object
   """
   def __init__(self, value, link_node=None):
-    self.value = value
-    self.link_node = link_node
+      self.value = value
+      self.link_node = link_node
+      
+  def get_value(self):
+      return self.value
+
+  def get_link_node(self):
+      return self.link_node
+
+  def set_link_node(self, link_node) -> None:
+      self.link_node = link_node
 
 ### DEMO Ring Structure ###
 node_1 = Node("test data")
 node_2 = Node(3)
-node_1.link_node = node_2
+node_1.set_link_node(node_2)
 node_3 = Node(6)
-node_2.link_node = node_3
+node_2.set_link_node(node_3)
 node_4 = Node("traversal about to loop", node_1)
-node_3.link_node = node_4
+node_3.set_link_node(node_4)
 
 iterations_count = 0
 current_node = node_1
 while type(current_node.link_node) != None and iterations_count < 10:
     iterations_count += 1
     print("Iteration: ", iterations_count)
-    print(current_node.value)
-    next_node = current_node.link_node
+    print(current_node.get_value())
+    next_node = current_node.get_link_node()
     current_node = next_node
 
