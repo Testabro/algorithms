616fd7f304ca71da7e54c33d3ad4e8e6ae0ec9e4
Tom Estabrook <thomas.r.estabrook@gmail.com>
2022-08-23 11:58:59 -0400

    add: linked list, add nodes to the head

---
+++ python/data-structures/linked-list.py
@@ -0,0 +1,53 @@
+class Node:
+  """ Basic generic building block for a data structure.
+
+  Attributes:
+    value: Undefined type as it is meant to be generic
+    next_node: A reference to a Node object
+  """
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+class LinkedList:
+  """ A collection of Nodes that are singularly linked with a defined begining Node
+  
+  Attributes:
+    head_node: A refernce to a Node that represents the begining of the linked list
+  """
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+
+### DEMO ###
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+print(ll.stringify_list())
