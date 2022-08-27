cccf2300a210c1e377e579810ccca8ec326fff6c
Tom Estabrook <thomas.r.estabrook@gmail.com>
2022-08-27 12:30:53 -0400

    feature: get a pointer to the middle node in the list

--- python/data-structures/linked-list.py
+++ python/data-structures/linked-list.py
@@ -106,6 +106,16 @@ def nth_last_node(linked_list, n):
         current = current.get_next_node()
   return current
 
+def find_middle(linked_list):
+  fast = linked_list.head_node
+  slow = linked_list.head_node
+  while fast:
+    fast = fast.get_next_node()
+    if fast:
+      fast = fast.get_next_node()
+      slow = slow.get_next_node()
+    return slow 
+
 ### DEMO ###
 ll = LinkedList(5)
 ll.insert_beginning(70)
